492f6e447ee00bee9cdf73b353992642f06e07ba
jopegi <josevi_1976@hotmail.com>
2017-11-22 13:28:45 +0100

    Actualización examen PSP Belén!

--- NavidadThread/src/NadidadThread.java
+++ NavidadThread/src/NadidadThread.java
@@ -1,49 +1,50 @@
 
-public class NadidadThread extends Thread{
-	
+public class NadidadThread extends Thread {
+
 	private String mensaje = null;
 	private String nombre;
-	
-	public NadidadThread (String nombre, String mensaje) {
-		
+
+	public NadidadThread(String nombre, String mensaje) {
+
 		this.nombre = nombre;
 		this.mensaje = mensaje;
 	}
 
-	public NadidadThread (String nombre) {
-		
-		this.mensaje = mensaje;
+	public NadidadThread(String nombre) {
+
+		this.nombre = nombre;
 	}
-	
+
 	@Override
 	public void run() {
-		
-		for (int j = 1; j<10; j++) {
-			
+
+		for (int j = 0; j < 10; j++) {
+
 			if (mensaje == null) {
-				
-				System.out.println("Estamos en la iteración: "+j);
-				
-			}else {
-				
+
+				System.out.println("Estamos en la iteración: " + j);
+
+			} else {
+
 				System.out.println(mensaje);
 			}
 			
-			System.out.println("Finaliza el hilo llamado: "+nombre);
 		}
 		
+		System.out.println("Finaliza el hilo llamado: " + nombre);
+
 	}
 
 	public static void main(String[] args) {
-		
-		Thread t1 = new NadidadThread ("hilo1" , "Feliz Navidad!");
-		Thread t2 = new NadidadThread ("hilo2" , "StarWars 8 is comming!");
-		Thread t3 = new NadidadThread ("hilo3");
-		
+
+		Thread t1 = new NadidadThread("hilo1", "Feliz Navidad!");
+		Thread t2 = new NadidadThread("hilo2", "StarWars 8 is comming!");
+		Thread t3 = new NadidadThread("hilo3");
+
 		t1.start();
 		t2.start();
 		t3.start();
-		
+
 		try {
 			
 			t1.join();
@@ -52,9 +53,9 @@ public class NadidadThread extends Thread{
 			
 		} catch (InterruptedException e) {
 			
+			
 			e.printStackTrace();
 		}
-
 
 		System.out.println("El hilo principal ha terminado y yo sin los polvorones comprados!!");
 	}
